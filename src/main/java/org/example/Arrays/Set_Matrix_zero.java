6c0dd3072ee6b69343250b401b977e61878382b7
neeloo patel <nilusingh945164@gmail.com>
2026-08-20 17:17:02 +0530

    dsa

--- src/main/java/org/example/Arrays/Set_Matrix_zero.java
+++ src/main/java/org/example/Arrays/Set_Matrix_zero.java
@@ -1,5 +1,7 @@
 package org.example.Arrays;
 
+import java.util.Arrays;
+
 public class Set_Matrix_zero {
     public static void main(String[] args) {
         int mat[][]={{1,1,1},{1,0,1},{1,1,1}};
@@ -20,6 +22,26 @@ public class Set_Matrix_zero {
                     mat[0][j]=0;
                 }
             }
+        }
+        for( int i =1;i<n;i++){
+            for( int j =1;j<m;j++){
+                if(mat[i][0]==0 || mat[0][j]==0){
+                    mat[i][j]=0;
+                }
+            }
+        }
+        if(fr){
+            for( int j =0;j<m;j++){
+                mat[0][j]=0;
+            }
+        }
+        if(fc){
+            for(int i =0; i <n;i++){
+                mat[i][0]=0;
+            }
+        }
+        for(int[] row : mat){
+            System.out.println(Arrays.toString(row));
         }
 
     }
