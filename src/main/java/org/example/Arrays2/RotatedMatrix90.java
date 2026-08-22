2bb33eb78dc60c6cd2abea6baee0935664a1c0a5
neeloo patel <nilusingh945164@gmail.com>
2026-08-22 17:13:03 +0530

    RotatedMatrix90 matrix

--- src/main/java/org/example/Arrays2/RotatedMatrix90.java
+++ src/main/java/org/example/Arrays2/RotatedMatrix90.java
@@ -6,6 +6,10 @@ import java.util.Arrays;
 public class RotatedMatrix90 {
     public static void main(String[] args) {
         int arr[][]= {{1,2,3},{4,5,6},{7,8,9}};
+        for( int a[]:arr){
+            System.out.println(Arrays.toString(a));
+        }
+        System.out.println("after rotation:");
         rotateMatrix(arr);
 
     }
