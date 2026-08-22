2a39411759cfafbd9d69b97184c72838f08e47e1
neeloo patel <nilusingh945164@gmail.com>
2026-08-22 17:50:52 +0530

    MergeTwoSortedarrauy

--- src/main/java/org/example/Arrays2/MergeSortedArray.java
+++ src/main/java/org/example/Arrays2/MergeSortedArray.java
@@ -19,58 +19,59 @@ public class MergeSortedArray {
         merge(a1, m, a2, n);
     }
 
-//     public void merge(int[] nums1, int m, int[] nums2, int n) {
-//         // Create a copy of the original nums1 elements to prevent overwriting them
-//         int[] nums1Copy = new int[m];
-//         for (int index = 0; index < m; index++) {
-//             nums1Copy[index] = nums1[index];
-//         }
+     public static void merge(int[] nums1, int m, int[] nums2, int n) {
+         // Create a copy of the original nums1 elements to prevent overwriting them
+         int[] nums1Copy = new int[m];
+         for (int index = 0; index < m; index++) {
+             nums1Copy[index] = nums1[index];
+         }
 
-//         int i = 0; // Pointer for nums1Copy
-//         int j = 0; // Pointer for nums2
-//         int k = 0; // Pointer for final nums1 array
+         int i = 0; // Pointer for nums1Copy
+         int j = 0; // Pointer for nums2
+         int k = 0; // Pointer for final nums1 array
 
-//         // Changed conditions to use correct bounds: i < m and j < n
-//         while (i < m && j < n) {
-//             if (nums1Copy[i] <= nums2[j]) {
-//                 nums1[k++] = nums1Copy[i++];
-//             } else {
-//                 nums1[k++] = nums2[j++];
-//             }
-//         }
+         // Changed conditions to use correct bounds: i < m and j < n
+         while (i < m && j < n) {
+             if (nums1Copy[i] <= nums2[j]) {
+                 nums1[k++] = nums1Copy[i++];
+             } else {
+                 nums1[k++] = nums2[j++];
+             }
+         }
 
-//         // Changed loop condition from <= to strictly < to avoid out of bounds
-//         while (i < m) {
-//             nums1[k++] = nums1Copy[i++];
-//         }
+         // Changed loop condition from <= to strictly < to avoid out of bounds
+         while (i < m) {
+             nums1[k++] = nums1Copy[i++];
+         }
 
-//         // Changed loop condition from <= to strictly < to avoid out of bounds
-//         while (j < n) {
-//             nums1[k++] = nums2[j++];
-//         }
-//     }
-// }
+         // Changed loop condition from <= to strictly < to avoid out of bounds
+         while (j < n) {
+             nums1[k++] = nums2[j++];
+         }
+         System.out.println(Arrays.toString(nums1));
+     }
+ }
 
-    public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1;         // Pointer for end of valid nums1 elements
-        int j = n - 1;         // Pointer for end of nums2 elements
-        int k = m + n - 1;     // Pointer for the very last index of nums1
-
-        // Merge from the highest values to the lowest values
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k--] = nums1[i--];
-            } else {
-                nums1[k--] = nums2[j--];
-            }
-        }
-
-        // If elements remain in nums2, copy them over
-        while (j >= 0) {
-            nums1[k--] = nums2[j--];
-        }
-        System.out.println("After merging:      " + Arrays.toString(nums1));
-
-        // Elements remaining in nums1 (i >= 0) are already in their correct places
-    }
-}
+//    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+//        int i = m - 1;         // Pointer for end of valid nums1 elements
+//        int j = n - 1;         // Pointer for end of nums2 elements
+//        int k = m + n - 1;     // Pointer for the very last index of nums1
+//
+//        // Merge from the highest values to the lowest values
+//        while (i >= 0 && j >= 0) {
+//            if (nums1[i] > nums2[j]) {
+//                nums1[k--] = nums1[i--];
+//            } else {
+//                nums1[k--] = nums2[j--];
+//            }
+//        }
+//
+//        // If elements remain in nums2, copy them over
+//        while (j >= 0) {
+//            nums1[k--] = nums2[j--];
+//        }
+//        System.out.println("After merging:      " + Arrays.toString(nums1));
+//
+//        // Elements remaining in nums1 (i >= 0) are already in their correct places
+//    }
+//}
