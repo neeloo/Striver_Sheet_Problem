0610c576e7b3a8d27e0d2ead2b928c861581a765
neeloo patel <nilusingh945164@gmail.com>
2026-08-22 16:55:59 +0530

    buy sell stock

--- src/main/java/org/example/Arrays/BuySellStock.java
+++ src/main/java/org/example/Arrays/BuySellStock.java
@@ -3,19 +3,19 @@ package org.example.Arrays;
 public class BuySellStock {
     public static void main(String[] args) {
         int arr[] = {3, 8, 1, 4, 6, 2};
-        System.out.println(stockBuySell(arr , arr.length));
+        System.out.println(stockBuySell(arr, arr.length));
 
     }
 
-        public static int stockBuySell(int[] arr, int n) {
-            int p =0;
-            int minP = arr[0];
-            for( int  i =0; i <n;i++){
-                minP = Math.min(arr[i],minP);
-                p =Math.max(p,arr[i]-minP);
-            }
-            return p;
-
+    public static int stockBuySell(int[] arr, int n) {
+        int p = 0;
+        int minP = arr[0];
+        for (int i = 1; i < n; i++) {
+            minP = Math.min(arr[i], minP);
+            p = Math.max(p, arr[i] - minP);
         }
+        return p;
+
+    }
 
 }
