61a4b940242cc230cc8c7c42e76276e15d80e133
neeloo patel <nilusingh945164@gmail.com>
2026-08-29 19:37:19 +0530

    MergeTwoSortedarrauy

--- src/main/java/org/example/Arrays2/FindDuplicate.java
+++ src/main/java/org/example/Arrays2/FindDuplicate.java
@@ -2,10 +2,12 @@ package org.example.Arrays2;
 
 public class FindDuplicate {
     public static void main(String[] args) {
+        int arr[]= {1,3,4,2,2};
+        System.out.println(findDuplicate(arr));
 
     }
 
-        public int findDuplicate(int[] nums) {
+        public static int findDuplicate(int[] nums) {
             for (int i = 0; i < nums.length; i++) {
                 int ind = Math.abs(nums[i]);
                 if (nums[ind] < 0) {
