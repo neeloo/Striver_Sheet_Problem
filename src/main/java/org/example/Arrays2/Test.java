092dfb11a155c5452b7c01dbcf13c818fc73b056
neeloo patel <nilusingh945164@gmail.com>
2026-08-31 22:34:42 +0530

    MergeTwoSortedarrauy

--- src/main/java/org/example/Arrays2/Test.java
+++ src/main/java/org/example/Arrays2/Test.java
@@ -1,10 +1,13 @@
 package org.example.Arrays2;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
         int arr[]={3, 5, 4, 1, 1};
         int ans [] = findMissingRepeatingNumbers(arr);
-
+        System.out.println(Arrays.toString(ans));
 
     }
     // class Solution {
