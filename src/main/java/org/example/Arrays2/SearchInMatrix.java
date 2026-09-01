9ddf66abeed044db36810bc1dbd690ad32e4970d
neeloo patel <nilusingh945164@gmail.com>
2026-09-02 00:01:01 +0530

    DSA

--- src/main/java/org/example/Arrays2/SearchInMatrix.java
+++ src/main/java/org/example/Arrays2/SearchInMatrix.java
@@ -4,4 +4,25 @@ public class SearchInMatrix {
     public static void main(String[] args) {
 
     }
+
+        public static boolean searchMatrix(int[][] m, int k) {
+            int row = m.length;
+            int col = m[0].length;
+            // top left conner
+            int i = row - 1;
+            int j = 0;
+            while (i >= 0 && j < col) {
+                if (m[i][j] == k) {
+                    return true;
+                }
+                if (m[i][j] < k) {
+                    j++;
+                } else {
+                    i--;
+                }
+            }
+            return false;
+        }
+
+
 }
