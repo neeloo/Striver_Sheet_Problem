784d2308263e285f4357eabaae8b2332d42d0706
neeloo patel <nilusingh945164@gmail.com>
2026-09-03 11:46:37 +0530

    DSA

--- src/main/java/org/example/Arrays2/Pow.java
+++ src/main/java/org/example/Arrays2/Pow.java
@@ -4,4 +4,25 @@ public class Pow {
     public static void main(String[] args) {
 
     }
+    public static double myPow(double x, int n) {
+        long N = n;
+        double ans = solve(x, Math.abs(N));
+        if (N < 0) {
+            return 1 / ans;
+        }
+        return ans;
+    }
+
+    public static double solve(double x, long n) {
+        if (n == 0) {
+            return 1;
+        }
+        double temp = solve(x, n / 2);
+        if (n % 2 == 0) {
+            return temp * temp;
+        } else {
+            return x * temp * temp;
+
+        }
+    }
 }
