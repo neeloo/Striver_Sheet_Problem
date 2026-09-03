98e94df70c957a09755c5409e73e60e7a2d07802
neeloo patel <nilusingh945164@gmail.com>
2026-09-03 12:00:47 +0530

    DSA

--- src/main/java/org/example/Arrays2/Majority1.java
+++ src/main/java/org/example/Arrays2/Majority1.java
@@ -2,6 +2,8 @@ package org.example.Arrays2;
 
 public class Majority1 {
     public static void main(String[] args) {
+        int nums[] = {2,2,1,1,1,2,2};
+        System.out.println(majorityElement(nums));
 
     }
 
