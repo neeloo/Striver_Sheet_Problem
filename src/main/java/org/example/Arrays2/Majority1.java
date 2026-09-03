bbd5b53e60b17725c4fe255d386d5ee2adf7588b
neeloo patel <nilusingh945164@gmail.com>
2026-09-03 11:59:58 +0530

    DSA

--- src/main/java/org/example/Arrays2/Majority1.java
+++ src/main/java/org/example/Arrays2/Majority1.java
@@ -4,4 +4,29 @@ public class Majority1 {
     public static void main(String[] args) {
 
     }
+
+    public static int majorityElement(int[] A) {
+        int majority = -1;
+        int frequency = 0;
+        int n = A.length;
+        for (int i = 0; i < n; i++) {
+            if (frequency == 0) {
+                majority = A[i];
+                frequency = 1;
+            } else if (A[i] == majority) {
+                frequency++;
+            } else {
+                frequency--;
+            }
+        }
+        frequency = 0; //(reset this)
+        for (int i = 0; i < n; i++) {
+            if (A[i] == majority) {
+                frequency++;
+            }
+        }
+        return frequency > (n / 2) ? majority : -1;
+
+    }
+
 }
