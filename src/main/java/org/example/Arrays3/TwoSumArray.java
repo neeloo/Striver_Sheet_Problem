88f3220a08cf0d397ef14c48e50e0e315c080b1a
neeloo patel <nilusingh945164@gmail.com>
2026-09-04 11:11:48 +0530

    DSA

--- src/main/java/org/example/Arrays3/TwoSumArray.java
+++ src/main/java/org/example/Arrays3/TwoSumArray.java
@@ -7,20 +7,22 @@ import java.util.Map;
 
 public class TwoSumArray {
     public static void main(String[] args) {
-        int nums[]={1, 6, 2, 10, 3};int target =7;
-        int ans[]= twoSum(nums , target);
+        int nums[] = {1, 6, 2, 10, 3};
+        int target = 7;
+        int ans[] = twoSum(nums, target);
         System.out.println(Arrays.toString(ans));
 
     }
+
     public static int[] twoSum(int[] nums, int target) {
-        Map<Integer , Integer> map = new HashMap<>();
-        for( int i=0;i<nums.length;i++){
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
             int contains = target - nums[i];
-            if( map.containsKey(contains)){
-                return new int[]{map.get(contains) , i};
+            if (map.containsKey(contains)) {
+                return new int[]{map.get(contains), i};
             }
-            map.put(nums[i] , i);
+            map.put(nums[i], i);
         }
-        return new int[]{-1,-1};
+        return new int[]{-1, -1};
     }
 }
