da0e8ba7b667c60425a2be2a09affdc31c8e7dc3
neeloo patel <nilusingh945164@gmail.com>
2026-09-04 11:07:42 +0530

    DSA

--- src/main/java/org/example/Arrays3/TwoSumArray.java
+++ src/main/java/org/example/Arrays3/TwoSumArray.java
@@ -1,7 +1,21 @@
 package org.example.Arrays3;
 
+import java.util.HashMap;
+
 public class TwoSumArray {
     public static void main(String[] args) {
+
+    }
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer , Integer> map = new HashMap<>();
+        for( int i=0;i<nums.length;i++){
+            int contains = target - nums[i];
+            if( map.containsKey(contains)){
+                return new int[]{map.get(contains) , i};
+            }
+            map.put(nums[i] , i);
+        }
+        return new int[]{-1,-1};
 
     }
 }
