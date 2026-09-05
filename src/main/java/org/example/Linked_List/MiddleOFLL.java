46176eabdd1ffcc4460d9ad3b1494c3a703fa271
neeloo patel <nilusingh945164@gmail.com>
2026-09-05 15:40:25 +0530

    LL

--- src/main/java/org/example/Linked_List/MiddleOFLL.java
+++ src/main/java/org/example/Linked_List/MiddleOFLL.java
@@ -25,7 +25,6 @@ public class MiddleOFLL {
     }
     public static void printLL(Node head) {
         Node temp = head;
-
         while (temp != null) {
             System.out.print(temp.val + " -> ");
             temp = temp.next;
