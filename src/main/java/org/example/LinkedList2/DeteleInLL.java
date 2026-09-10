4b6ebc398a9ea5a60990f0ec6e9cce273090301d
neeloo patel <nilusingh945164@gmail.com>
2026-09-10 11:46:33 +0530

    LL

---
+++ src/main/java/org/example/LinkedList2/DeteleInLL.java
@@ -0,0 +1,33 @@
+package org.example.LinkedList2;
+
+public class DeteleInLL {
+
+    public static void main(String[] args) {
+
+        // Create linked list: 1 -> 2 -> 3 -> 4 -> null
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+
+        boolean result = deleteInLL(head);
+
+        System.out.println(result);
+    }
+
+    public static boolean deleteInLL(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
