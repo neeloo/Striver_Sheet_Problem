acd4256c6449622f5c8d1fc9c48dab17ea430272
neeloo patel <nilusingh945164@gmail.com>
2026-09-14 12:56:19 +0530

    LL

--- src/main/java/org/example/Binary_Search/SingleElement.java
+++ src/main/java/org/example/Binary_Search/SingleElement.java
@@ -2,6 +2,15 @@ package org.example.Binary_Search;
 
 public class SingleElement {
     public static void main(String[] args) {
+        int nums[]={1, 1, 2, 2, 3, 3, 4, 5, 5, 6, 6};
+        System.out.println(findSingleElement(nums));
 
+    }
+    public static int findSingleElement(int arr[]){
+        int xor =0;
+        for( int a:arr){
+            xor ^= a;
+        }
+        return xor;
     }
 }
