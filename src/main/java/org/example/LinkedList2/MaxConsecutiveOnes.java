59d04759248795f36750d9c1d44b93a746514caa
neeloo patel <nilusingh945164@gmail.com>
2026-09-14 12:53:33 +0530

    LL

--- src/main/java/org/example/LinkedList2/MaxConsecutiveOnes.java
+++ src/main/java/org/example/LinkedList2/MaxConsecutiveOnes.java
@@ -1,7 +1,7 @@
 package org.example.LinkedList2;
 
 public class MaxConsecutiveOnes {
-    public  static int findMaxConsecutiveOnes(int[] nums) {
+    public static int findMaxConsecutiveOnes(int[] nums) {
         int maxCount = 0, count = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == 1) {
@@ -16,7 +16,7 @@ public class MaxConsecutiveOnes {
     }
 
     public static void main(String[] args) {
-        int nums[]={1,1,0,1,1,1};
+        int nums[] = {1, 1, 0, 1, 1, 1};
         System.out.println(findMaxConsecutiveOnes(nums));
     }
 }
