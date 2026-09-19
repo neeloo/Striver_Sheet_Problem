150ac5cfd02510f14a8fb805efb26ef8a626dd8e
neeloo patel <nilusingh945164@gmail.com>
2026-09-19 16:52:50 +0530

    string

---
+++ src/main/java/org/example/Strings/LongestPalidrom.java
@@ -0,0 +1,33 @@
+package org.example.Strings;
+
+class LongestPalidrome {
+   public static int maxlen = 0;
+    public  static int len = 0;
+
+    public static String longestPalindrome(String s) {
+        int n = s.length();
+        if (n < 2)
+            return s;
+        for (int i = 0; i < n; i++) {
+            expand(s, i, i); //for odd length substring
+            expand(s, i, i + 1); //even length substring
+        }
+        return s.substring(len, len + maxlen);
+    }
+
+    public  static void expand(String s, int a, int b) {
+        while (a >= 0 && b < s.length() && s.charAt(a) == s.charAt(b)) {
+            a--;
+            b++;
+        }
+        if (maxlen < b - a - 1) { // b-a+1-2 (return if not substring)
+            maxlen = b - a - 1;
+            len = a + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+        String s = "babad";
+        System.out.println(longestPalindrome(s));
+    }
+}
---
+++ src/main/java/org/example/Strings/ReverseStringWord.java
@@ -0,0 +1,25 @@
+package org.example.Strings;
+
+public class ReverseStringWord {
+    public static void main(String[] args) {
+        String s = "the sky is blue";
+        // Call the manual character reversal method
+        System.out.println(reverseManual(s));
+    }
+
+    public static String reverseManual(String s) {
+        // Handle edge case if the string is empty or null
+        if (s == null || s.isEmpty()) {
+            return s;
+        }
+
+        StringBuilder res = new StringBuilder();
+
+        // Start from the last index (length - 1) and move backwards to 0
+        for (int i = s.length() - 1; i >= 0; i--) {
+            res.append(s.charAt(i)); // Grab each character one by one
+        }
+
+        return res.toString();
+    }
+}
