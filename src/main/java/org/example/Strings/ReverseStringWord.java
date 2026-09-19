4a416dc38742870dc9214ef23447b3f2f05b9528
neeloo patel <nilusingh945164@gmail.com>
2026-09-19 15:52:31 +0530

    string

--- src/main/java/org/example/Strings/ReverseStringWord.java
+++ src/main/java/org/example/Strings/ReverseStringWord.java
@@ -3,21 +3,20 @@ package org.example.Strings;
 public class ReverseStringWord {
     public static void main(String[] args) {
         String s = "the sky is blue";
-        System.out.println(s);
+        System.out.println(reversestring(s));
 
     }
-    public  static String reversestring(String s){
-        String st[] = s.split("//s+");
-        StringBuilder sb = new StringBuilder();
-        for( int i =  st.length;i>=0;i--){
-            sb.append(st[i]);
-            if(i != 0){
-                sb.append(" ");
-            }
 
-        }
-        return sb.toString().trim();
+    public static String reversestring(String s) {
+        String[] words = s.split("\\s+");
+         StringBuilder res = new StringBuilder();
 
-
+         for (int i = words.length - 1; i >= 0; i--) {
+             res.append(words[i]);
+             if (i != 0) {
+                 res.append(" ");
+             }
+         }
+         return res.toString().trim();
     }
 }
