0a03e0fc54e23d19e7000ddd77d424428d208ff0
neeloo patel <nilusingh945164@gmail.com>
2026-09-22 23:24:50 +0530

    string

--- src/main/java/org/example/DP/Stairs.java
+++ src/main/java/org/example/DP/Stairs.java
@@ -2,6 +2,10 @@ package org.example.DP;
 
 public class Stairs {
     public static void main(String[] args) {
+        int
 
+    }
+    public  static  int stairs(int n){
+        return stairs(n-1)+ stairs(n-2);
     }
 }
