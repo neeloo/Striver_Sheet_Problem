7f0b0eaeeb4dcd6eafe5643b17bab9ef31ec0d86
neeloo patel <nilusingh945164@gmail.com>
2026-09-22 23:30:34 +0530

    string

--- src/main/java/org/example/DP/Stairs.java
+++ src/main/java/org/example/DP/Stairs.java
@@ -2,12 +2,13 @@ package org.example.DP;
 
 public class Stairs {
     public static void main(String[] args) {
-        int n =3; ///(1,1,1) , (1,2),(2,1)
+        int n = 3; ///(1,1,1) , (1,2),(2,1)
         System.out.println(stairs(n));
 
     }
-    public  static  int stairs(int n){
-        if (n==0 || n==1)return 1;
-        return stairs(n-1)+ stairs(n-2);
+
+    public static int stairs(int n) {
+        if (n == 0 || n == 1) return 1;
+        return stairs(n - 1) + stairs(n - 2);
     }
 }
