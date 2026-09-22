15bb249b46db49d0e5756adb2f85403d535d4cce
neeloo patel <nilusingh945164@gmail.com>
2026-09-22 19:08:39 +0530

    string

--- src/main/java/org/example/Strings/ReverseStringWord.java
+++ src/main/java/org/example/Strings/ReverseStringWord.java
@@ -14,7 +14,6 @@ public class ReverseStringWord {
             return s;
         }
         StringBuilder res = new StringBuilder();
-
         // Start from the last index (length - 1) and move backwards to 0
         for (int i = s.length() - 1; i >= 0; i--) {
             res.append(s.charAt(i)); // Grab each character one by one
