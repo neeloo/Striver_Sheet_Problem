be9563a65c0d3418ee4784eea03cbaa800433c9d
neeloo patel <nilusingh945164@gmail.com>
2026-09-25 12:25:33 +0530

    string

--- src/main/java/org/example/DP/FrogJump.java
+++ src/main/java/org/example/DP/FrogJump.java
@@ -5,15 +5,16 @@ import java.util.Arrays;
 class FrogJump {
     public int frogJump(int[] heights) {
         int n = heights.length;
-        int dp[]= new int[heights.length +1];
-        Arrays.fill(dp , -1);
+        int dp[] = new int[heights.length + 1];
+        Arrays.fill(dp, -1);
 
-        return minCostMemo( n-1 , heights , dp);
-        
+        return minCostMemo(n - 1, heights, dp);
+
     }
-    public static int minCostMemo(int n , int[]heights ,  int dp[]){
-        if(n ==0)return 0;
-         // If there are only 2 stairs(0-based 
+
+    public static int minCostMemo(int n, int[] heights, int dp[]) {
+        if (n == 0) return 0;
+        // If there are only 2 stairs(0-based
         // index), then frog can only take 
         // jump of size one
         if (n == 1)
@@ -21,10 +22,10 @@ class FrogJump {
 
         // if the result for this subproblem is 
         // already computed then return it
-        if(dp[n] != -1)return dp[n];
+        if (dp[n] != -1) return dp[n];
 
-        dp[n]=Math.min(minCostMemo(n-1 , heights , dp)+Math.abs(heights[n] - heights[n-1]) ,
-                        minCostMemo(n-2 , heights, dp) + Math.abs(heights[n] - heights[n-2]));
+        dp[n] = Math.min(minCostMemo(n - 1, heights, dp) + Math.abs(heights[n] - heights[n - 1]),
+                minCostMemo(n - 2, heights, dp) + Math.abs(heights[n] - heights[n - 2]));
 
         return dp[n];
     }
